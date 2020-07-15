f0494058b8a06445cc87a792ab3edeee23d65c58
Maria Artymovych <mashkaart94@gmail.com>
2020-07-15 12:28:11 +0300

    line-endings

---
+++ public/js/chat.js
@@ -0,0 +1,81 @@
+const socket = io();
+
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $messages = document.querySelector("#messages");
+const $locations = document.querySelector("#location-link");
+
+//templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+
+function htmlToElement(html) {
+  var template = document.createElement("template");
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message,
+  });
+  const htmlElement = htmlToElement(html);
+  $messages.insertAdjacentElement("beforeend", htmlElement);
+});
+
+socket.on("locationMessage", (url) => {
+  console.log(url);
+  const html = Mustache.render(locationTemplate, {
+    url,
+  });
+  const htmlElement = htmlToElement(html);
+  $locations.insertAdjacentElement("beforeend", htmlElement);
+});
+
+const messageForm = document.querySelector("form");
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //disable
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target[0].value;
+
+  socket.emit("sendMessage", message, (error) => {
+    //enable
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message delivered");
+  });
+});
+
+const $locationButton = document.querySelector("#send-location");
+
+$locationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("geolocation is not supported");
+  }
+  $locationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const data = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
+    };
+
+    socket.emit("sendLocation", data, (message) => {
+      $locationButton.removeAttribute("disabled");
+      console.log(message);
+    });
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const http = require("http");
+const chalk = require("chalk");
+const path = require("path");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+
+const port = process.env.PORT || 3001;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
+
+const locationUrl ="https://google.com/maps?q=,"
+
+io.on("connection", (socket) => {
+  console.log("New websocket connection");
+
+  socket.emit("message", "Welcome");
+  socket.broadcast.emit("message", "A new user has joined"); // sends message to everyone exept current connection
+
+  
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed");
+    }
+
+    io.emit("message", message);
+    callback();
+  });
+
+  socket.on("sendLocation", (data, callback) => {
+    io.emit("locationMessage", `https://google.com/maps?q=${data.lat},${data.long}`);
+    callback("Delivered");
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  }); // disconnect - built-in event
+});
+
+server.listen(port, () => {
+  console.log(chalk.greenBright("Server is up on port" + port));
+});
